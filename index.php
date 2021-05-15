6a6f221221923abc18481294f6c3daacb3bec2bf
Seen Technologies <seentechs@localhost.localdomain>
2021-05-15 19:12:25 -0400

    initial project
    initial project

---
+++ index.php
@@ -0,0 +1,283 @@
+
+<!DOCTYPE html>
+<html lang="en">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<title>FLATKICK</title>
+<meta charset="utf-8">    
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="img/favicon.php" type="image/x-icon">
+<link rel="shortcut icon" href="img/favicon.php" type="image/x-icon" />
+<meta name="description" content="Your description">
+<meta name="keywords" content="Your keywords">
+<meta name="author" content="Your name">
+<meta name = "format-detection" content = "telephone=no" />
+<!--CSS-->
+<link rel="stylesheet" href="css/bootstrap.css" >
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/camera.css">
+<link href="../maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<!-- Start WOWSlider.com HEAD section -->
+<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+<script type="text/javascript" src="engine1/jquery.js"></script>
+<!-- End WOWSlider.com HEAD section -->
+<!--JS-->
+<script src="js/jquery.js"></script>
+<script src="js/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+
+<script src="js/jquery.equalheights.js"></script> 
+<script src="js/camera.js"></script> 
+<script src='http://maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false'></script>
+<script>
+    $(document).ready(function(){
+        jQuery('.camera_wrap').camera();
+    });
+</script>
+<!--[if (gt IE 9)|!(IE)]><!-->
+<script src="js/wow/wow.js"></script>
+<script src="js/wow/device.min.js"></script>
+<script src="js/jquery.mobile.customized.min.js"></script>
+<script>
+    $(document).ready(function () {       
+      if ($('html').hasClass('desktop')) {
+        new WOW().init();
+      }   
+    });
+</script>
+<!--<![endif]-->
+
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+
+    <div id="ie6-alert" style="width: 100%; text-align:center;">
+    <img src="http://beatie6.frontcube.com/images/ie6.jpg" alt="Upgrade IE 6" width="640" height="344" border="0" usemap="#Map" longdesc="http://die6.frontcube.com" />
+      <map name="Map" id="Map"><area shape="rect" coords="496,201,604,329" href="http://www.microsoft.com/windows/internet-explorer/default.aspx" target="_blank" alt="Download Interent Explorer" /><area shape="rect" coords="380,201,488,329" href="http://www.apple.com/safari/download/" target="_blank" alt="Download Apple Safari" /><area shape="rect" coords="268,202,376,330" href="http://www.opera.com/download/" target="_blank" alt="Download Opera" /><area shape="rect" coords="155,202,263,330" href="http://www.mozilla.com/" target="_blank" alt="Download Firefox" />
+        <area shape="rect" coords="35,201,143,329" href="http://www.google.com/chrome" target="_blank" alt="Download Google Chrome" />
+      </map>
+  </div>
+  <![endif]-->
+</head>
+<body>
+<!--header-->
+<header>
+    <div class="box">
+        <div class="container">
+		<div class="row">
+			<div class="box0 col-md-6">
+				<div class="row">
+				<div class="pull-left">
+				<img src="img/logo.png">
+				</div>
+				<div>
+				<strong class="navbar-brand navbar-brand_ wow fadeInDown"></strong>
+				</div>
+				</div>
+			</div>	
+            <div class="box1 col-md-6">
+                
+            </div>
+		</div>	
+        </div>
+    </div>
+    <div class="">
+        <nav class="navbar navbar-default navbar-static-top tm_navbar clearfix" role="navigation">
+		        <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+
+            <span class="sr-only">Toggle navigation</span>
+
+            <span class="icon-bar"></span>
+
+            <span class="icon-bar"></span>
+
+            <span class="icon-bar"></span>
+
+        </button>
+		<div class="container">
+		<div id="navbarCollapse" class="collapse navbar-collapse">
+            <ul class="nav sf-menu clearfix">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="register.php">Jackpot</a></li>
+                <li><a href="register.php">Multi Bets</a></li>
+                <li><a href="#">Results</a></li>
+                <li><a href="register.php">Join</a></li>
+            </ul>
+		</div>	
+		</div>
+        </nav>
+    </div>
+</header>    
+<!-- Start WOWSlider.com BODY section -->
+<div id="wowslider-container1">
+<div class="ws_images"><ul>
+		<li><img src="data1/images/slide_2.jpg" alt="" title="" id="wows1_0"/></li>
+		<li><img src="data1/images/15525167318522.jpg" alt="15525167318522" title="15525167318522" id="wows1_1"/></li>
+		<li><img src="data1/images/slide_3.jpg" alt="slide (3)" title="slide (3)" id="wows1_2"/></li>
+		<li><a href="#"><img src="data1/images/slide_4.jpg" alt="javascript photo gallery" title="slide (4)" id="wows1_3"/></a></li>
+		<li><img src="data1/images/slide_5.jpg" alt="slide (5)" title="slide (5)" id="wows1_4"/></li>
+	</ul></div>
+<div class="ws_script" style="position:absolute;left:-99%"><a href="#">image slider</a> by WOWSlider.com v8.8</div>
+<div class="ws_shadow"></div>
+</div>	
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+<!-- End WOWSlider.com BODY section -->
+<div class="content"> 
+    
+    
+    <div class="thumb-box2">
+        <div class="container">
+                    
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 wow fadeInUp ">
+					<h3 class="title"><u>Welcome to FLATKICK For accurate tips</u></h3>
+				
+                    <div class="pull-left">
+					<span>
+					<i class="fa fa-check-circle fa-1x" style="color: #695cb8;"></i>
+					</span>
+					</div>
+					<div>
+						We are the Kenya’s Largest Betting tips platform providing the best Jackpot and Multi Bet Tips, we help you make money.
+					</div>
+					<h3 class="title"><u>Why Choose Us</u></h3>
+				
+                    <div class="pull-left">
+					<span>
+					<i class="fa fa-check-circle fa-1x" style="color: #695cb8;"></i>
+					</span>
+					</div>
+					<div>
+						We have been in this industry for many years , over this time we have acquired all the skills required in sports betting.
+					</div>
+					<div class="pull-left">
+					<span>
+					<i class="fa fa-check-circle fa-1x" style="color: #695cb8;"></i>
+					</span>
+					</div>
+					<div>
+						Our predictions are reliable . We undertake thorough research and prove that they are working before we share them with you.
+					</div>
+					<div class="pull-left">
+					<span>
+					<i class="fa fa-check-circle fa-1x" style="color: #695cb8;"></i>
+					</span>
+					</div>
+					<div>
+						At FLATKICK we offer jackpot predictions and Multi Bets. Read more about each product below.
+					</div>
+				
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <p>
+					<img src="img/pesa.jpg"  style="width: 100%; height: auto;max-height: 350px;">
+					</p>
+                    
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="charges">
+		<div class="container">
+			<div class="row">
+				<div class="package package-pro col-md-6">
+					<table class="table info">
+						<thead class="head">
+							<tr>
+								<th colspan="2">
+									<p class="plan">Jackpot</p>
+									<p class="price"><span>KSH</span>80</p>
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr><td>Winning Jackpot can always take several attempts before a success .</td></tr>
+							<tr><td>but We have a strategy that solves this.</td></tr>
+							<tr><td>For each Jackpot we offer <b>2</b> different predictions. You can miss one but win with the other.</td></tr>
+							<tr><td>For the two predictions we charge <b>KSHS.80.</b> Mpesa <b>Till NO: 899647.</b></td></tr>
+							<tr><td>Predictions will be sent via <b>SMS</b> immediately after payment.</td></tr>
+						</tbody>
+						<tfoot>
+							<tr><td colspan="2"><a href="register.php" class="btn signup">Join Flatkicks
+							now!</a></td></tr>
+						</tfoot>
+					</table>
+				</div>
+				<div class="package package-basic col-md-6">
+					<table class="table info">
+						<thead class="head">
+							<tr>
+								<th colspan="2">
+									<p class="plan">MULTI-BETS</p>
+									<p class="price"><span>KSH</span>150</p>
+								</th>
+							</tr>
+						</thead>
+						<tbody>
+							<tr><td>We do all the best to make our Multi Bets the safest and most profitable.</td></tr>
+							<tr><td>We have <b>3 Mid-week</b> Multi Bets and <b>3 weekend</b> Multi Bets.</td></tr>
+							<tr><td>The Mid-week Multi bets are compiled from the safest matches played from Monday-Friday.</td></tr>
+							<tr><td>The weekend Multi Bets are compiled from the safest matches played from Saturday-Sunday.</td></tr>
+							<tr><td>With this strategy you can always <b>win 6/6</b> multi bets in a week.</td></tr>
+							<tr><td>For each group of multi bets we charge <b>KSHS.150</b>. Mpesa <b>Till NO: 899647</b>.</td></tr>
+						</tbody>
+						<tfoot>
+							<tr><td colspan="2"><a href="register.php" class="btn signup">Join Flatkicks
+							now!</a></td></tr>
+						</tfoot>
+					</table>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="thumb-box2">
+        <div class="container">
+            <p class="title wow fadeIn">How to Register</p>
+            <p>
+						<ol>
+						 <li>Go to <b>Mpesa</b>, Select <b>Lipa Na M-PESA</b> and then select <b>Buy Goods</b>.</li>
+						 <li>Enter Our Till No.-<b>899647</b>.</li>
+						 <li>Enter the Amount Ksh.150(MULTI-BET). ksh.80(JACKPOT). <b>Pay Exact amount</b></li>
+						 <li>Confirm  to complete the transaction.</li>
+						 <li>You will receive a confirmation of Payment to <b>Flatkicks.</b></li>
+						 <li>The tips will been sent via SMS immediately after payment</li>
+						 <li>By registering you agree to our terms and condition.</li>
+						</ol>
+						</p>
+        </div>
+    </div>
+    
+<div class="thumb-box7">
+        <div class="container">
+            <div class="col-lg-6 col-md-6 col-sm-6 wow fadeInUp">
+                <p class="title">contacts</p>
+                <p><strong>Phone:</strong>0716312183,  0769427507</p>
+                <p><strong>Hours:</strong> 8am-5pm  Monday-Friday;   8am-1pm Saturday</p>
+               <p><strong>Postal Code:</strong> 00600 Nairobi</a></p>
+                <p><strong>E-mail:</strong> <a href="#">info@flatkicks.co.ke</a></p>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <section class="content_map wow zoomIn">
+                      <div class="google-map-api"> 
+                        <div id="map-canvas" class="gmap"></div> 
+                      </div> 
+                </section>
+            </div>
+        </div>
+    </div>
+<!--footer-->
+<footer> 
+    <div class="container">
+           <p class="wow fadeInUp"> &copy;2019  Flatkicks Powered By<em id="copyright-year"></em>. <a href="tirotech.co.ke">Tiro Tech Solutions</a></p>
+    </div>
+    </div>
+  <!-- {%FOOTER_LINK} -->
+</footer>
+<script src="js/bootstrap.min.js"></script>
+
+</body>
+
+</html>	
---
+++ register.php
@@ -0,0 +1,262 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<meta http-equiv="content-type" content="text/html;charset=UTF-8" />
+<head>
+<title>FLATKICK</title>
+<meta charset="utf-8">    
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<link rel="icon" href="img/favicon.php" type="image/x-icon">
+<link rel="shortcut icon" href="img/favicon.php" type="image/x-icon" />
+<meta name="description" content="Your description">
+<meta name="keywords" content="Your keywords">
+<meta name="author" content="Your name">
+<meta name = "format-detection" content = "telephone=no" />
+<!--CSS-->
+<link rel="stylesheet" href="css/bootstrap.css" >
+<link rel="stylesheet" href="css/style.css">
+<link rel="stylesheet" href="css/animate.css">
+<link rel="stylesheet" href="css/camera.css">
+<link href="../maxcdn.bootstrapcdn.com/font-awesome/4.1.0/css/font-awesome.min.css" rel="stylesheet">
+<!-- Start WOWSlider.com HEAD section -->
+<link rel="stylesheet" type="text/css" href="engine1/style.css" />
+<script type="text/javascript" src="engine1/jquery.js"></script>
+<!-- End WOWSlider.com HEAD section -->
+<!--JS-->
+<script src="js/jquery.js"></script>
+<script src="js/jquery-migrate-1.2.1.min.js"></script>
+<script src="js/jquery.easing.1.3.js"></script>
+
+<script src="js/jquery.equalheights.js"></script> 
+<script src="js/camera.js"></script> 
+<script src='http://maps.googleapis.com/maps/api/js?v=3.exp&amp;sensor=false'></script>
+<script>
+    $(document).ready(function(){
+        jQuery('.camera_wrap').camera();
+    });
+</script>
+<!--[if (gt IE 9)|!(IE)]><!-->
+<script src="js/wow/wow.js"></script>
+<script src="js/wow/device.min.js"></script>
+<script src="js/jquery.mobile.customized.min.js"></script>
+<script>
+    $(document).ready(function () {       
+      if ($('html').hasClass('desktop')) {
+        new WOW().init();
+      }   
+    });
+</script>
+<!--<![endif]-->
+
+<!--[if lt IE 9]>
+    <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+    <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+
+    <div id="ie6-alert" style="width: 100%; text-align:center;">
+    <img src="http://beatie6.frontcube.com/images/ie6.jpg" alt="Upgrade IE 6" width="640" height="344" border="0" usemap="#Map" longdesc="http://die6.frontcube.com" />
+      <map name="Map" id="Map"><area shape="rect" coords="496,201,604,329" href="http://www.microsoft.com/windows/internet-explorer/default.aspx" target="_blank" alt="Download Interent Explorer" /><area shape="rect" coords="380,201,488,329" href="http://www.apple.com/safari/download/" target="_blank" alt="Download Apple Safari" /><area shape="rect" coords="268,202,376,330" href="http://www.opera.com/download/" target="_blank" alt="Download Opera" /><area shape="rect" coords="155,202,263,330" href="http://www.mozilla.com/" target="_blank" alt="Download Firefox" />
+        <area shape="rect" coords="35,201,143,329" href="http://www.google.com/chrome" target="_blank" alt="Download Google Chrome" />
+      </map>
+  </div>
+  <![endif]-->
+</head>
+<body>
+<!--header-->
+<header>
+    <div class="box">
+        <div class="container">
+		<div class="row">
+			<div class="box0 col-md-6">
+				<div class="row">
+				<div class="pull-left">
+				<img src="img/logo.png">
+				</div>
+				<div>
+				<strong class="navbar-brand navbar-brand_ wow fadeInDown"></strong>
+				</div>
+				</div>
+			</div>	
+            <div class="box1 col-md-6">
+                
+            </div>
+		</div>	
+        </div>
+    </div>
+    <div class="">
+        <nav class="navbar navbar-default navbar-static-top tm_navbar clearfix" role="navigation">
+		        <button type="button" data-target="#navbarCollapse" data-toggle="collapse" class="navbar-toggle">
+
+            <span class="sr-only">Toggle navigation</span>
+
+            <span class="icon-bar"></span>
+
+            <span class="icon-bar"></span>
+
+            <span class="icon-bar"></span>
+
+        </button>
+		<div class="container">
+		<div id="navbarCollapse" class="collapse navbar-collapse">
+            <ul class="nav sf-menu clearfix">
+                <li><a href="index.php">Home</a></li>
+                <li><a href="register.php">Jackpot</a></li>
+                <li><a href="register.php">Multi Bets</a></li>
+                <li><a href="#">Results</a></li>
+                <li><a href="register.php">Join</a></li>
+            </ul>
+		</div>	
+		</div>
+        </nav>
+    </div>
+</header>    
+<!-- Start WOWSlider.com BODY section -->
+<div id="wowslider-container1">
+<div class="ws_images"><ul>
+		<li><img src="data1/images/slide_2.jpg" alt="" title="" id="wows1_0"/></li>
+		<li><img src="data1/images/15525167318522.jpg" alt="15525167318522" title="15525167318522" id="wows1_1"/></li>
+		<li><img src="data1/images/slide_3.jpg" alt="slide (3)" title="slide (3)" id="wows1_2"/></li>
+		<li><a href="#"><img src="data1/images/slide_4.jpg" alt="javascript photo gallery" title="slide (4)" id="wows1_3"/></a></li>
+		<li><img src="data1/images/slide_5.jpg" alt="slide (5)" title="slide (5)" id="wows1_4"/></li>
+	</ul></div>
+<div class="ws_script" style="position:absolute;left:-99%"><a href="#">image slider</a> by WOWSlider.com v8.8</div>
+<div class="ws_shadow"></div>
+</div>	
+<script type="text/javascript" src="engine1/wowslider.js"></script>
+<script type="text/javascript" src="engine1/script.js"></script>
+<!-- End WOWSlider.com BODY section -->
+<div class="content"> 
+    
+    
+    <div class="thumb-box2">
+        <div class="container">
+            <p class="title wow fadeIn">How to Register</p>
+            <div class="row">
+                <div class="col-lg-6 col-md-6 col-sm-6 wow fadeInUp ">
+				<div class="row">
+                    <div class="pull-left">
+					<span>
+					<i class="fa fa-check-circle fa-1x" style="color: #695cb8;"></i>
+					</span>
+					</div>
+					<div>
+					<p>
+					</i>On your mobile Device  go to <b>M-PESA menu</b>
+					</p>
+					</div>
+				</div>
+				<div class="row">
+                    <div class="pull-left">
+					<span>
+					<i class="fa fa-check-circle fa-1x" style="color: #695cb8;"></i>
+					</span>
+					</div>
+					<div>
+					<p>
+					</i>Select<b>Lipa Na M-PESA</b> and then select <b>Buy Goods and Services.</b>
+					</p>
+					</div>
+				</div>
+				<div class="row">
+                    <div class="pull-left">
+					<span>
+					<i class="fa fa-check-circle fa-1x" style="color: #695cb8;"></i>
+					</span>
+					</div>
+					<div>
+					<p>
+					</i>Enter our  Till Number <b>899647</b>
+					</p>
+					</div>
+				</div>
+				<div class="row">
+                    <div class="pull-left">
+					<span>
+					<i class="fa fa-check-circle fa-1x" style="color: #695cb8;"></i>
+					</span>
+					</div>
+					<div>
+					<p>
+					</i></i>Enter the Amount <b>Ksh. 80</b> (Jackpot)/<b>Ksh. 200</b> (Multi Bet)
+					</p>
+					</div>
+				</div>
+				<div class="row">
+                    <div class="pull-left">
+					<span>
+					<i class="fa fa-check-circle fa-1x" style="color: #695cb8;"></i>
+					</span>
+					</div>
+					<div>
+					<p>
+					</i>Confirm Payment to complete Transaction.
+					</p>
+					</div>
+				</div>
+				<div class="row">
+                    <div class="pull-left">
+					<span>
+					<i class="fa fa-check-circle fa-1x" style="color: #695cb8;"></i>
+					</span>
+					</div>
+					<div>
+					<p>
+					</i>You will receive a confirmation of the transaction to <b>Flatkicks</b> from <b>M-PESA</b>, and a text from <b>Flatkicks</b> containing your predictions Within seconds
+					</p>
+					</div>
+				</div>
+				<div class="row">
+                    <div class="pull-left">
+					<span>
+					<i class="fa fa-check-circle fa-1x" style="color: #695cb8;"></i>
+					</span>
+					</div>
+					<div>
+					<p>
+					</i>By registering you agree to our terms and condition
+					</p>
+					</div>
+				</div>
+                </div>
+                <div class="col-lg-6 col-md-6 col-sm-6 wow fadeInUp" data-wow-delay="0.1s">
+                    <p>
+					<img src="img/till.png"  style="width: 100%; height: auto;max-height: 250px;">
+					</p>
+                    
+                </div>
+            </div>
+            
+        </div>
+    </div>
+	
+    
+<div class="thumb-box7">
+        <div class="container">
+            <div class="col-lg-6 col-md-6 col-sm-6 wow fadeInUp">
+                <p class="title">contacts</p>
+                <p><strong>Phone:</strong>0716312183,  0769427507</p>
+                <p><strong>Hours:</strong> 8am-5pm  Monday-Friday;   8am-1pm Saturday</p>
+               <p><strong>Postal Code:</strong> 00600 Nairobi</a></p>
+                <p><strong>E-mail:</strong> <a href="#">info@flatkicks.co.ke</a></p>
+            </div>
+            <div class="col-lg-6 col-md-6 col-sm-6">
+                <section class="content_map wow zoomIn">
+                      <div class="google-map-api"> 
+                        <div id="map-canvas" class="gmap"></div> 
+                      </div> 
+                </section>
+            </div>
+        </div>
+    </div>
+<!--footer-->
+<footer> 
+    <div class="container">
+           <p class="wow fadeInUp"> &copy;2019  FlatkicksPowered By<em id="copyright-year"></em>. <a href="tirotech.co.ke">Tiro Tech Solutions</a></p>
+    </div>
+    </div>
+  <!-- {%FOOTER_LINK} -->
+</footer>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/validator.php"></script>
+</body>
+
+</html>	
